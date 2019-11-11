a89687d565a237adecc70ee5fa464853e8ae659c
rkshkmr800 <rkshkmr800@gmail.com>
2019-11-11 15:45:33 +0900

    added UI for home screen and for input to database

--- server/handlers.go
+++ server/handlers.go
@@ -1,7 +1,22 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func hello(c *gin.Context) {
-	c.String(200, "Hello World")
+	c.HTML(http.StatusOK, "index.html", gin.H{})
+}
+
+func outputAPI(c *gin.Context) {
+	c.HTML(http.StatusOK, "output.html", gin.H{"title": "Take a Test"})
+}
+
+func inputForm(c *gin.Context) {
+	//c.HTML(http.StatusOK, "inputForm.html", gin.H{"title": "Take a Test"})
+	japaneseWord := c.PostForm("japaneseWord")
+	englishWord := c.PostForm("englishWord")
+	c.String(200, japaneseWord+"###"+englishWord)
 }
