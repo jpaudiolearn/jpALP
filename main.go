d2e4bcde044404fd7705c1c749aacda9885ccd12
mohitpokharna <mohitpokharna08@gmail.com>
2019-11-18 20:06:29 +0900

    Login created

--- main.go
+++ main.go
@@ -6,16 +6,16 @@ import (
 
 func main() {
 	router := server.CreateRouter()
-	// client := db.GetClient()
-	// cl, err := db.LoadTextColl(client, "./db/config.yml")
-	// if err != nil {
-	// 	fmt.Println("could not load" + err.Error())
-	// 	return
-	// }
-	// err = db.InitTextColl(cl)
-	// if err != nil {
-	// 	fmt.Println("could not load" + err.Error())
-	// 	return
-	// }
+	client := db.GetClient()
+	cl, err := db.LoadTextColl(client, "./db/config.yml")
+	if err != nil {
+		fmt.Println("could not load" + err.Error())
+	 	return
+	}
+	err = db.InitTextColl(cl)
+	if err != nil {
+		fmt.Println("could not load" + err.Error())
+		return
+	}
 	server.StartServer(router)
 }
