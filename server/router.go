10cc54dbb6c8386bd45577352e3f90e5b98d3bcb
sidhhant <sidhhant.t20@gmail.com>
2019-11-18 18:32:08 +0900

    frontend commented

--- server/router.go
+++ server/router.go
@@ -8,11 +8,11 @@ func setupRoutes(router *gin.Engine) {
 
 	// Mapping html and css files to gin
 	//fmt.Println(gin.Mode())
-	if mode := gin.Mode(); mode == gin.DebugMode {
-		router.LoadHTMLGlob("./templates/*")
-	} else {
-		router.LoadHTMLGlob("./templates/*")
-	}
+	// if mode := gin.Mode(); mode == gin.DebugMode {
+	// 	router.LoadHTMLGlob("./templates/*")
+	// } else {
+	// 	router.LoadHTMLGlob("./templates/*")
+	// }
 	router.Static("/css", "./static/css")
 	router.Static("/js", "./static/js")
 	router.Static("/media", "./static/media")
